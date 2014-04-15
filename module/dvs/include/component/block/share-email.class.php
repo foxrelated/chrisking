ffd6eb8762df5ed38ccf8ab79681c236bd782311
WheelsTV <wheelstv@wtvdvs.com>
2014-04-15 12:28:01 -0500

    share link 2.0 fixes

--- module/dvs/include/component/block/share-email.class.php
+++ module/dvs/include/component/block/share-email.class.php
@@ -20,7 +20,8 @@ class Dvs_Component_Block_Share_Email extends Phpfox_Component {
         ->setHeader(array('share_email.css' => 'style_css'))
 				->assign(array(
 					'aVideo' => Phpfox::getService('dvs.video')->get($this->request()->get('sRefId')),
-					'aDvs' => Phpfox::getService('dvs')->get($this->request()->getInt('iDvsId'), false)
+					'aDvs' => Phpfox::getService('dvs')->get($this->request()->getInt('iDvsId'), false),
+          'bLongUrl' => $this->request()->getInt('longurl', false),
 		));
 	}
 
