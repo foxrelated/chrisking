49ab891e448b443e6350fa6b5cbb09200db63788
Collin Davis <cdavis@wheelstv.net>
2014-04-29 14:27:45 -0700

    Removed return false from footer link

--- module/dvs/template/default/controller/view.html.php
+++ module/dvs/template/default/controller/view.html.php
@@ -222,7 +222,7 @@ defined('PHPFOX') or exit('No direct script access allowed.');
       <ul>
         {foreach from=$aFooterLinks key=iKey item=aVideo name=videos}
         <li>
-          <a href="{if $bSubdomainMode}{url link=$aDvs.title_url}{else}{url link='dvs.'$aDvs.title_url}{/if}{$aVideo.video_title_url}" onclick="menuFooter('Footer Link Clicks'); return false;">
+          <a href="{if $bSubdomainMode}{url link=$aDvs.title_url}{else}{url link='dvs.'$aDvs.title_url}{/if}{$aVideo.video_title_url}" onclick="menuFooter('Footer Link Clicks');">
             {$aVideo.year} {$aVideo.make} {$aVideo.model}
           </a>
           {/foreach}
