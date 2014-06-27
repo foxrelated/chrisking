e516ad1832d9cb3ccd9e62d6925ea59f67a78f2d
Collin Davis <cdavis@wheelstv.net>
2014-06-27 14:36:10 -0700

    fixed video load on mobile and updated code formatting of player desktop and mobile templates

--- module/dvs/template/default/controller/player/player.html.php
+++ module/dvs/template/default/controller/player/player.html.php
@@ -17,98 +17,99 @@ defined('PHPFOX') or exit('No direct script access allowed.');
 	var aMediaIds = [];
 			var aOverviewMediaIds = [];
 			var aTestDriveMediaIds = [];
+	
 	{if $bIsDvs}
 
 	{foreach from = $aOverviewVideos key = iKey item = aVideo}
-	aOverviewMediaIds[{$iKey}] = {$aVideo.id};
+		aOverviewMediaIds[{$iKey}] = {$aVideo.id};
 	{/foreach}
 
-			aMediaIds = aOverviewMediaIds;
+	aMediaIds = aOverviewMediaIds;
+	
 	{if isset($aOverrideVideo.id)}
-	if (bDebug) console.log('Media: Override is set. aMediaIds:');
-			aMediaIds[0] = {$aOverrideVideo.id};
-	{ else}
-	{if isset($aFeaturedVideo.id)}
-	if (bDebug) console.log('Media: Featured Video is set. aMediaIds:');
-			aMediaIds[0] = {$aFeaturedVideo.id};
-	{ else}
-	if (bDebug) console.log('Media: No override or featuerd. aMediaIds:');
+		if (bDebug) console.log('Media: Override is set. aMediaIds:');
+		aMediaIds[0] = {$aOverrideVideo.id};
+	{else}
+		{if isset($aFeaturedVideo.id)}
+			if (bDebug) console.log('Media: Featured Video is set. aMediaIds:');
+				aMediaIds[0] = {$aFeaturedVideo.id};
+			{else}
+			if (bDebug) console.log('Media: No override or featuerd. aMediaIds:');
 			aMediaIds = aOverviewMediaIds;
-	{/if}
-	{/if}
-			if (bDebug) {l}
-	console.log(aMediaIds);
+			{/if}
+		{/if}
+	if (bDebug) {l}
+		console.log(aMediaIds);
 	{r}
 
 	{if $aPlayer.custom_overlay_1_type}
-	if (bDebug) console.log('Overlay: Overlay 1 is active. Type: {$aPlayer.custom_overlay_1_type}. Start: {$aPlayer.custom_overlay_1_start}. Duration: {$aPlayer.custom_overlay_1_duration}.');
-			var bCustomOverlay1 = true;
-			var iCustomOverlay1Start = {$aPlayer.custom_overlay_1_start};
-			var iCustomOverlay1Duration = {$aPlayer.custom_overlay_1_duration};
-	{ else}
-	var bCustomOverlay1 = false;
-			if (bDebug) console.log('Overlay: Overlay 1 is inactive.');
+		if (bDebug) console.log('Overlay: Overlay 1 is active. Type: {$aPlayer.custom_overlay_1_type}. Start: {$aPlayer.custom_overlay_1_start}. Duration: {$aPlayer.custom_overlay_1_duration}.');
+		var bCustomOverlay1 = true;
+		var iCustomOverlay1Start = {$aPlayer.custom_overlay_1_start};
+		var iCustomOverlay1Duration = {$aPlayer.custom_overlay_1_duration};
+	{else}
+		var bCustomOverlay1 = false;
+		if (bDebug) console.log('Overlay: Overlay 1 is inactive.');
 	{/if}
 
 	{if $aPlayer.custom_overlay_2_type}
-	if (bDebug) console.log('Overlay: Overlay 2 is active. Type: {$aPlayer.custom_overlay_2_type}. Start: {$aPlayer.custom_overlay_2_start}. Duration: {$aPlayer.custom_overlay_2_duration}.');
-			var bCustomOverlay2 = true;
-			var iCustomOverlay2Start = {$aPlayer.custom_overlay_2_start};
-			var iCustomOverlay2Duration = {$aPlayer.custom_overlay_2_duration};
-	{ else}
-	var bCustomOverlay2 = false;
-			if (bDebug) console.log('Overlay: Overlay 2 is inactive.');
+		if (bDebug) console.log('Overlay: Overlay 2 is active. Type: {$aPlayer.custom_overlay_2_type}. Start: {$aPlayer.custom_overlay_2_start}. Duration: {$aPlayer.custom_overlay_2_duration}.');
+		var bCustomOverlay2 = true;
+		var iCustomOverlay2Start = {$aPlayer.custom_overlay_2_start};
+		var iCustomOverlay2Duration = {$aPlayer.custom_overlay_2_duration};
+	{else}
+		var bCustomOverlay2 = false;
+		if (bDebug) console.log('Overlay: Overlay 2 is inactive.');
 	{/if}
 
 	{if $aPlayer.custom_overlay_3_type}
-	if (bDebug) console.log('Overlay: Overlay 3 is active. Type: {$aPlayer.custom_overlay_3_type}. Start: {$aPlayer.custom_overlay_3_start}. Duration: {$aPlayer.custom_overlay_3_duration}.');
-			var bCustomOverlay3 = true;
-			var iCustomOverlay3Start = {$aPlayer.custom_overlay_3_start};
-			var iCustomOverlay3Duration = {$aPlayer.custom_overlay_3_duration};
-	{ else}
-	var bCustomOverlay3 = false;
-			if (bDebug) console.log('Overlay: Overlay 3 is inactive.');
-	{/if}
-
-
-	{ else}
-	{foreach from = $aVideos key = iKey item = aVideo}
-	aMediaIds[{$iKey}] = {$aVideo.id};
-	{/foreach}
-
-	{if isset($aFeaturedVideo.id)}
-	aMediaIds[0] = {$aFeaturedVideo.id};
-	{/if}
-	{/if}
-
-	{if !$bIsExternal}
-	var bPreRoll = {if $aPlayer.preroll_file_id}true{ else}false{/if};
-			var iDvsId = {if $bIsDvs}{$iDvsId}{ else}0{/if};
-			var bIdriveGetPrice = {if !$bIsDvs && isset($aPlayer.email) && $aPlayer.email}true{ else}false{/if};
-			var bPreview = {if $bPreview}true{ else}false{/if};
-			var bAutoplay = {if (isset($aPlayer.autoplay) && $aPlayer.autoplay) || (isset($aPlayer.autoplay_baseurl) && $aPlayer.autoplay_baseurl && !$aBaseUrl) || (isset($aPlayer.autoplay_videourl) && $aPlayer.autoplay_videourl && $aBaseUrl)}true{ else}false{/if};
-			//var bAutoplay =true;
-			var iCurrentVideo = {$aCurrentVideo};
-			var bAutoAdvance = {if isset($aPlayer.autoadvance) && $aPlayer.autoadvance}true{ else}false{/if};
+		if (bDebug) console.log('Overlay: Overlay 3 is active. Type: {$aPlayer.custom_overlay_3_type}. Start: {$aPlayer.custom_overlay_3_start}. Duration: {$aPlayer.custom_overlay_3_duration}.');
+		var bCustomOverlay3 = true;
+		var iCustomOverlay3Start = {$aPlayer.custom_overlay_3_start};
+		var iCustomOverlay3Duration = {$aPlayer.custom_overlay_3_duration};
 	{else}
-	var bPreRoll = false;
-			var iDvsId = 0;
-			var bIdriveGetPrice = {if $bShowGetPrice}true{ else}false{/if};
-			var bPreview = false;
-			var bAutoplay = {if $bAutoplay}true{ else}false{/if};
-			var bAutoAdvance = true;
+		var bCustomOverlay3 = false;
+		if (bDebug) console.log('Overlay: Overlay 3 is inactive.');
 	{/if}
 
-			function setPlayerStyle(){l}
-	if (bDebug)
-	{l}
-	console.log("Player: Setting player style and volume.");
-			modVid.setVolume(0);
-	{r}
+	{else}
+		{foreach from = $aVideos key = iKey item = aVideo}
+			aMediaIds[{$iKey}] = {$aVideo.id};
+		{/foreach}
+
+		{if isset($aFeaturedVideo.id)}
+			aMediaIds[0] = {$aFeaturedVideo.id};
+		{/if}
+	{/if}
 
 	{if !$bIsExternal}
-	modVid.setStyles('titleText-active:#{$aPlayer.player_text};titleText-disabled:#{$aPlayer.player_text};titleText-rollover:#{$aPlayer.player_text};titleText-selected:#{$aPlayer.player_text};bodyText-active:#{$aPlayer.player_text};bodyText-disabled:#{$aPlayer.player_text};bodyText-rollover:#{$aPlayer.player_text};bodyText-selected:#{$aPlayer.player_text};buttons-icons:#{$aPlayer.player_button_icons};buttons-rolloverIcons:#{$aPlayer.player_button_icons};buttons-selectedIcons:#{$aPlayer.player_button_icons};buttons-glow:#{$aPlayer.player_button_icons};buttons-iconGlow:#{$aPlayer.player_button_icons};buttons-face:#{$aPlayer.player_buttons};buttons-rollover:#{$aPlayer.player_buttons};buttons-selected:#{$aPlayer.player_buttons};playheadWell-background:#{$aPlayer.player_progress_bar};playheadWell-watched:#{$aPlayer.player_progress_bar};playhead-face:#{$aPlayer.player_button_icons};volumeControl-icons:#{$aPlayer.player_button_icons};volumeControl-track:#{$aPlayer.player_progress_bar};volumeControl-face:#{$aPlayer.player_buttons};linkText-active:#{$aPlayer.player_text};linkText-disabled:#{$aPlayer.player_text};linkText-rollover:#{$aPlayer.player_text};linkText-downState:#{$aPlayer.player_text};');
+		var bPreRoll = {if $aPlayer.preroll_file_id}true{else}false{/if};
+		var iDvsId = {if $bIsDvs}{$iDvsId}{else}0{/if};
+		var bIdriveGetPrice = {if !$bIsDvs && isset($aPlayer.email) && $aPlayer.email}true{else}false{/if};
+		var bPreview = {if $bPreview}true{else}false{/if};
+		var bAutoplay = {if (isset($aPlayer.autoplay) && $aPlayer.autoplay) || (isset($aPlayer.autoplay_baseurl) && $aPlayer.autoplay_baseurl && !$aBaseUrl) || (isset($aPlayer.autoplay_videourl) && $aPlayer.autoplay_videourl && $aBaseUrl)}true{ else}false{/if};
+		//var bAutoplay =true;
+		var iCurrentVideo = {$aCurrentVideo};
+		var bAutoAdvance = {if isset($aPlayer.autoadvance) && $aPlayer.autoadvance}true{ else}false{/if};
+	{else}
+		var bPreRoll = false;
+		var iDvsId = 0;
+		var bIdriveGetPrice = {if $bShowGetPrice}true{else}false{/if};
+		var bPreview = false;
+		var bAutoplay = {if $bAutoplay}true{else}false{/if};
+		var bAutoAdvance = true;
 	{/if}
+
+	function setPlayerStyle(){l}
+		if (bDebug)
+		{l}
+			console.log("Player: Setting player style and volume.");
+			modVid.setVolume(0);
+		{r}
+
+		{if !$bIsExternal}
+			modVid.setStyles('titleText-active:#{$aPlayer.player_text};titleText-disabled:#{$aPlayer.player_text};titleText-rollover:#{$aPlayer.player_text};titleText-selected:#{$aPlayer.player_text};bodyText-active:#{$aPlayer.player_text};bodyText-disabled:#{$aPlayer.player_text};bodyText-rollover:#{$aPlayer.player_text};bodyText-selected:#{$aPlayer.player_text};buttons-icons:#{$aPlayer.player_button_icons};buttons-rolloverIcons:#{$aPlayer.player_button_icons};buttons-selectedIcons:#{$aPlayer.player_button_icons};buttons-glow:#{$aPlayer.player_button_icons};buttons-iconGlow:#{$aPlayer.player_button_icons};buttons-face:#{$aPlayer.player_buttons};buttons-rollover:#{$aPlayer.player_buttons};buttons-selected:#{$aPlayer.player_buttons};playheadWell-background:#{$aPlayer.player_progress_bar};playheadWell-watched:#{$aPlayer.player_progress_bar};playhead-face:#{$aPlayer.player_button_icons};volumeControl-icons:#{$aPlayer.player_button_icons};volumeControl-track:#{$aPlayer.player_progress_bar};volumeControl-face:#{$aPlayer.player_buttons};linkText-active:#{$aPlayer.player_text};linkText-disabled:#{$aPlayer.player_text};linkText-rollover:#{$aPlayer.player_text};linkText-downState:#{$aPlayer.player_text};');
+		{/if}
 	{r}
 
 	function enableVideoSelectCarousel(){l}
@@ -157,7 +158,7 @@ defined('PHPFOX') or exit('No direct script access allowed.');
 					scroll: 3,
 					speed: 900
 			{r});
-		{ else}
+		{else}
 			$("#overview_playlist").jCarouselLite({l}
 				btnNext: ".next",
 					btnPrev: ".prev",
@@ -263,11 +264,14 @@ defined('PHPFOX') or exit('No direct script access allowed.');
 		{/if}
 	</object>
 	{/if}
-	{literal}<script type="text/javascript">
+	{literal}
+	<script type="text/javascript">
 		$Behavior.brightCoveCreateExp = function()
 		{
 			brightcove.createExperiences();
-		}</script>{/literal}
+		}
+	</script>
+	{/literal}
 	{if $bIsDvs || (!$bIsExternal && !$aPlayer.player_type) || ($bIsExternal && $bShowPlaylist)}
 	<section id="playlist_wrapper{if $inventoryList} inventory_wrapper{/if}">
 		{if $aDvs.inv_display_status}
