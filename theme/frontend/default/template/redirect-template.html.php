5f65d565fc109e78498429ed16959ea0b6021ef0
phpfoxplus <salem280575@gmail.com>
2014-07-13 17:33:57 +0700

    Create new template for redirect page

--- theme/frontend/default/template/redirect-template.html.php
+++ theme/frontend/default/template/redirect-template.html.php
@@ -15,8 +15,8 @@ defined('PHPFOX') or exit('NO DICE!');
 <head>
     <meta property="og:title" content="{$aFirstVideoMeta.name}" />
     <meta property="og:site_name" content="WheelsTV" />
-    <meta property="og:description" content="" />
-    <meta property="og:image" content="{$aFirstVideoMeta.description}" />
+    <meta property="og:description" content="{$aFirstVideoMeta.description}" />
+    <meta property="og:image" content="{$aFirstVideoMeta.thumbnail_url}" />
     <meta property="article:author" content="{$aDvs.dealer_user_full_name}" />
     <title>{title}</title>
 </head>
