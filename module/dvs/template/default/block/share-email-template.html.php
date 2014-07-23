38ad83286626f4aa791c725734e1e173a81a0230
Collin Davis <cdavis@wheelstv.net>
2014-07-23 14:34:38 -0700

    updated email template 2 column layout

--- module/dvs/template/default/block/share-email-template.html.php
+++ module/dvs/template/default/block/share-email-template.html.php
@@ -11,16 +11,10 @@
 							<a href="{$sVideoLink}">{img path='core.url_file' file='dvs/branding/'.$aDvs.branding_file_name style="vertical-align:middle" max_width=600 max_height=300 suffix='_600'}</a>
 						</td>
 					</tr>
+					
 					<tr>
-						<td valign="top">
-							Hello {$sShareName}!<br />
-						</td>
-						<td valign="top" style="text-align:center;">
-							<a href="{$sVideoLink}" style="text-decoration:none;font-weight:bold;">Take a {$aVideo.year} {$aVideo.make} {$aVideo.model} Test Drive</a><br />
-						</td>
-					</tr>
-					<tr>
-						<td valign="top" width="50%">
+						<td valign="top" width="60%">
+							Hello {$sShareName}!<br /><br />
 							Take a <a href="{$sVideoLink}" style="text-decoration:none;font-weight:bold;">{$aVideo.year} {$aVideo.make} {$aVideo.model} Video Test Drive</a> from {$aDvs.dealer_name} -- It's fun, easy, and free!<br><br>
 							Your friend {$sMyShareName} has this to say:<br />
 							"{$sShareMessage}"
@@ -28,7 +22,8 @@
 							<br />
 							Drive Safely!<br />
 							{$aDvs.dealer_name}</td>
-						<td valign="top" width="50%">
+						<td valign="top" width="40%">
+						<a href="{$sVideoLink}" style="text-decoration:none;font-weight:bold;">Take a {$aVideo.year} {$aVideo.make} {$aVideo.model} Test Drive</a><br />
 							<div style="position: relative;width:160px;overflow:hidden">
 								<a href="{$sVideoLink}">
 									<div style="display: block; width: 160px; height: 90px; background: url({img path='core.url_file' file='brightcove/'.$aVideo.thumbnail_image return_url=true}) scroll no-repeat top left transparent;">
