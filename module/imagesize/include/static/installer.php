209efaadcf4df434ac2b5a3fc91b60cc5504f891
phpfoxplus <salem280575@gmail.com>
2014-07-24 18:01:01 +0700

    Complete imagesize module

---
+++ module/imagesize/include/static/installer.php
@@ -0,0 +1,3 @@
+ALTER TABLE `phpfox_ko_brightcove` ADD `image_path` VARCHAR( 75 ) NULL DEFAULT NULL ,
+ADD `image_server_id` TINYINT( 3 ) NOT NULL DEFAULT '0',
+ADD `is_resize` TINYINT( 1 ) NOT NULL DEFAULT '0'
