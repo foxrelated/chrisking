b9f77ca67f869729863277b4964c479fa593c5dd
WheelsTV <wheelstv@wtvdvs.com>
2014-08-26 16:21:41 -0500

    added plain text email share template

---
+++ module/dvs/include/component/block/share-email-plain-template.class.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * [PHPFOX_HEADER]
+ */
+defined('PHPFOX') or exit('No direct script access allowed.');
+
+/**
+ *
+ *
+ * @copyright		Konsort.org 
+ * @author  		Konsort.org
+ * @package 		DVS
+ */
+class Dvs_Component_Block_Share_Email_Plain_Template extends Phpfox_Component {
+
+	public function process()
+	{
+		$sReferenceId = $this->getParam('sReferenceId');
+		$iDvsId = $this->getParam('iDvsId');
+		
+		$aForms = Phpfox::getService('dvs.style')->get($iDvsId);
+		
+		$selected = Phpfox::getService('dvs.style')->getFontFamilies();
+		$ses='';
+		foreach($selected as $ik=>$sele)
+		{
+			if($ik == $aForms['font_family_id'])
+			{
+				$ses = $sele;
+				break;
+			}
+		}
+		
+		$this->template()
+			->assign(array(
+				'aVideo' => Phpfox::getService('dvs.video')->get($sReferenceId),
+				'aDvs' => Phpfox::getService('dvs')->get($iDvsId, false),
+				'sShareName' => $this->getParam('sShareName'),
+				'sMyShareName' => $this->getParam('sMyShareName'),
+				'sShareMessage' => $this->getParam('sShareMessage'),
+				'sBackgroundImageUrl' => $this->getParam('sBackgroundImageUrl'),
+				'sVideoLink' => $this->getParam('sVideoLink'),
+				'sImagePath' => $this->getParam('sImagePath'),
+				'sShareEmail' => $this->getParam('sShareEmail'),
+				'aForms'	=> $aForms,
+				'ses'		=> $ses
+		));
+	}
+
+
+}
+
+?>
---
+++ module/dvs/template/default/block/share-email-plain-template.html.php
@@ -0,0 +1,7 @@
+Hello {$sShareName}!
+Take a {$aVideo.year} {$aVideo.make} {$aVideo.model} Video Test Drive from {$aDvs.dealer_name} -- It's fun, easy, and free!
+Copy and Paste this URL to watch:{$sVideoLink}
+Your friend {$sMyShareName} has this to say:
+"{$sShareMessage}"
+Drive Safely!
+ {$aDvs.dealer_name}
