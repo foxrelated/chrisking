87207041ea94d13ab486696a947727c667dc4749
phpfoxplus <salem280575@gmail.com>
2014-10-10 15:27:25 +0700

    Build inventory importer

--- module/dvs/include/component/controller/test.class.php
+++ module/dvs/include/component/controller/test.class.php
@@ -4,9 +4,9 @@ class Dvs_Component_Controller_Test extends Phpfox_Component {
     public function process() {
         //Phpfox::getService('dvs.inventory')->importFile();
 
-        Phpfox::getService('dvs.inventory')->updateEdStyleId();
+        //Phpfox::getService('dvs.inventory')->updateEdStyleId();
 
-        //Phpfox::getService('dvs.inventory')->updateReferenceId();
+        Phpfox::getService('dvs.inventory')->updateReferenceId(30);
     }
 }
 ?>
