43cb32d5ef8269c87c7de510200873a9b6e4913c
Collin Davis <cdavis@wheelstv.net>
2015-08-12 18:29:51 -0700

    fixed font size

--- module/dvs/template/default/block/deactive.html.php
+++ module/dvs/template/default/block/deactive.html.php
@@ -3,7 +3,7 @@
         <a href="http://dealervideoshowroom.com">{img theme='wtvdvs/dealer_logo.png' max_width='600'}</a>
         <p></p>
         <p>To re-activate Virtual Test Drives:</p>
-        <p>Call <a href="tel:+18449433578">1-844-943-3578</a></p>
-        <p>Email <a href="mailto:support@dealervideoshowroom.com">support@dealervideoshowroom.com</a></p>
+        <p style="font-size:20px;">Call <a href="tel:+18449433578">1-844-943-3578</a></p>
+        <p style="font-size:20px;">Email <a href="mailto:support@dealervideoshowroom.com">support@dealervideoshowroom.com</a></p>
     </div>
 </div>
