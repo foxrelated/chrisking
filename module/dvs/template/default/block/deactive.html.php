d2d7cb56fc76847f388674cb7c34a8cea7a73799
Collin Davis <cdavis@wheelstv.net>
2015-08-12 18:23:53 -0700

    updated deactive messaging

--- module/dvs/template/default/block/deactive.html.php
+++ module/dvs/template/default/block/deactive.html.php
@@ -1,6 +1,6 @@
 <div id="deactive_popup_wrapper">
     <div id="deactive_popup_content">
-        {img theme='wtvdvs/dealer_logo.png' max_width='600'}
-        <p>To re-activate Virtual Test Drives call <a href="tel:+18449433578">1-844-943-3578</a> or email <a href="mailto:support@dealervideoshowroom.com">support@dealervideoshowroom.com</a></span></p>
+        <a href="http://dealervideoshowroom.com">{img theme='wtvdvs/dealer_logo.png' max_width='600'}</a>
+        <br /><p>To re-activate Virtual Test Drives call <a href="tel:+18449433578">1-844-943-3578</a> or email <a href="mailto:support@dealervideoshowroom.com">support@dealervideoshowroom.com</a></span></p>
     </div>
 </div>
