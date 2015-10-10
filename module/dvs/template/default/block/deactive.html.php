582fb25c4c5d45c15251c231c0ff073cc93c7914
Collin Davis <cdavis@wheelstv.net>
2015-10-10 02:19:54 -0700

    adjusted logo size

--- module/dvs/template/default/block/deactive.html.php
+++ module/dvs/template/default/block/deactive.html.php
@@ -1,6 +1,6 @@
 <div id="deactive_popup_wrapper">
     <div id="deactive_popup_content">
-        <a href="http://dealervideoshowroom.com">{img theme='wtvdvs/dealer_logo.jpg' max_width='600'}</a>
+        <a href="http://dealervideoshowroom.com">{img theme='wtvdvs/dealer_logo.jpg' max_width='400'}</a>
         <p></p>
         <p>To re-activate Virtual Test Drives:</p>
         <p style="font-size:20px;">Call <a href="tel:+18449433578">1-844-943-3578</a></p>
