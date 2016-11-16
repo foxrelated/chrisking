d014e4183cb203dc9acb3c09e0c2bf6c7acf027e
WheelsTV <wheelstv@wtvdvs.com>
2016-11-16 15:12:02 -0600

    updated de-activated message

--- module/dvs/template/default/block/deactive.html.php
+++ module/dvs/template/default/block/deactive.html.php
@@ -1,9 +1,10 @@
 <div id="deactive_popup_wrapper">
     <div id="deactive_popup_content">
-        <a href="http://dealervideoshowroom.com">{img theme='wtvdvs/dealer_logo.jpg' max_width='400'}</a>
+        <a href="http://dealervideoshowroom.com/?utm_medium=popup&utm_campaign=dvs_deactivated_popup&utm_source=website_link" target="_blank">{img theme='wtvdvs/dealer_logo.jpg' max_width='300'}</a>
         <p></p>
-        <p>Contact the dealership to re-activate Virtual Test Drives:</p>
-        <p style="font-size:20px;">Call: <a href="tel:+{$aDvs.phone}"><span id="deactive_phone">{$aDvs.phone}</span></a></p>
-        {*<p style="font-size:20px;">Email: <a href="mailto:{$aDvs.email}">{$aDvs.email}</a></p>*}
+        <p>Want to (re)activate Virtual Test Drives for your dealership?</p>
+        <p style="font-size:18px;">Call: <a href="tel:+18449433578?utm_medium=popup&utm_campaign=dvs_deactivated_popup&utm_source=phone_link">1-844-943-3578</a><br/>
+        Email: <a href="mailto:sales@dealervideoshowroom.com?utm_medium=popup&utm_campaign=dvs_deactivated_popup&utm_source=email_link">sales@dealervideoshowroom.com</a><br/>
+        Visit: <a href="http://dealervideoshowroom.com/?utm_medium=popup&utm_campaign=dvs_deactivated_popup&utm_source=website_link" target="_blank">www.dealervideoshowroom.com</a></p>
     </div>
 </div>
