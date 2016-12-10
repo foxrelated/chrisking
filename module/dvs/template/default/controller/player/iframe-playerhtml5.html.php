292cc5aff63fd3eb6ea89bd17d5636f1a4bb4bb9
Collin Davis <cdavis@wheelstv.net>
2016-12-09 17:45:19 -0800

    iframe player html5 fix

--- module/dvs/template/default/controller/player/iframe-playerhtml5.html.php
+++ module/dvs/template/default/controller/player/iframe-playerhtml5.html.php
@@ -486,7 +486,7 @@ function enableVideoSelectCarousel(){l}
     <button class="next playlist-button">&gt;</button>
 </section>
 {/if}
-<!--<script src="//players.brightcove.net/607012070001/0d15f8a3-b382-44ca-a53b-51870dd2ad3f_default/index.min.js"></script> -->
-<script src="//players.brightcove.net/607012070001/default_default/index.min.js"></script> 
+<script src="//players.brightcove.net/607012070001/0d15f8a3-b382-44ca-a53b-51870dd2ad3f_default/index.min.js"></script>
+<!-- <script src="//players.brightcove.net/607012070001/default_default/index.min.js"></script> --> 
 <script type="text/javascript" src="https://players.brightcove.net/videojs-custom-endscreen/dist/videojs-custom-endscreen.min.js"></script>
 <script src="//players.brightcove.net/videojs-overlay/lib/videojs-overlay.js"></script>
