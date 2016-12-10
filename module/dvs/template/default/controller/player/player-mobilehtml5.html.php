b976138ec93ed280c03b1a4a787de1c13bd1ab43
Collin Davis <cdavis@wheelstv.net>
2016-12-09 17:46:15 -0800

    mobilehtml5 player fix

--- module/dvs/template/default/controller/player/player-mobilehtml5.html.php
+++ module/dvs/template/default/controller/player/player-mobilehtml5.html.php
@@ -405,7 +405,7 @@ color:#fff;
         <div class="player_error">{phrase var='dvs.no_videos_error'}</div>
     {/if}
 </div>
-<!--<script src="//players.brightcove.net/607012070001/0d15f8a3-b382-44ca-a53b-51870dd2ad3f_default/index.min.js"></script>--> 
-<script src="//players.brightcove.net/607012070001/default_default/index.min.js"></script>
+<script src="//players.brightcove.net/607012070001/0d15f8a3-b382-44ca-a53b-51870dd2ad3f_default/index.min.js"></script> 
+<!-- <script src="//players.brightcove.net/607012070001/default_default/index.min.js"></script> -->
 <script type="text/javascript" src="https://players.brightcove.net/videojs-custom-endscreen/dist/videojs-custom-endscreen.min.js"></script>
 <script src="//players.brightcove.net/videojs-overlay/lib/videojs-overlay.js"></script>
