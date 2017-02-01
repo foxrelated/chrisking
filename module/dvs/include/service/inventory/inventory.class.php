ab61bc056ed5ceeb99019cd3e646b33c5086d607
Collin Davis <cdavis@wheelstv.net>
2017-02-01 11:13:49 -0800

    updated SFTP login and path

--- module/dvs/include/service/inventory/inventory.class.php
+++ module/dvs/include/service/inventory/inventory.class.php
@@ -6,10 +6,10 @@ class Dvs_Service_Inventory_Inventory extends Phpfox_Service {
     //private $_sUsername = 'WTVMain';
     //private $_sPassword = '$new123';
 	
-	private $_sHost = 'feeds.dealervideoshowroom.com';
-    private $_sPort = '21';
-    private $_sUsername = 'feedimport@dealervideoshowroom.com';
-    private $_sPassword = 'P)n~4pC6b-hE';
+	private $_sHost = 'dealervideoshowroom.com';
+    private $_sPort = '22';
+    private $_sUsername = 'dvs@dealervideoshowroom.com';
+    private $_sPassword = 'wh33l5tvh0trod';
 	
     function __construct() {
         $this->_sTable = Phpfox::getT('tbd_dvs_inventory');
@@ -328,7 +328,7 @@ class Dvs_Service_Inventory_Inventory extends Phpfox_Service {
         }
 
 
-        if (!$oDir = opendir("ssh2.sftp://{$oStream}/./")) {
+        if (!$oDir = opendir("ssh2.sftp://{$oStream}/public_html/feeds/output/")) {
             die('Could not open the directory');
             return false;
         }
