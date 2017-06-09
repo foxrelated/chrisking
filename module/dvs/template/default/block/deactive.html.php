66c34bacf3d619e3fcf07810cd7e03cfb5d98c53
Collin Davis <collin.e.davis@gmail.com>
2017-06-09 00:17:48 -0700

    updated deactive messaging

--- module/dvs/template/default/block/deactive.html.php
+++ module/dvs/template/default/block/deactive.html.php
@@ -2,7 +2,7 @@
     <div id="deactive_popup_content">
         <a href="http://dealervideoshowroom.com/?utm_medium=popup&utm_campaign=dvs_deactivated_popup&utm_source=website_link" target="_blank">{img theme='wtvdvs/dealer_logo.jpg' max_width='300'}</a>
         <p></p>
-        <p>Contact {$aDvs.dealer_name} to take a WheelsTV Virtual Test Drive:</p>
+        <p>Contact {$aDvs.dealer_name} to take a Virtual Test Drive from WheelsTV:</p>
         <p style="font-size:20px;">Call: <a href="tel:+{$aDvs.phone}?utm_medium=popup&utm_campaign=dvs_deactivated_popup&utm_source=phone_link"><span id="deactive_phone">{$aDvs.phone}</span></a></p>
         <p style="font-size:20px;">Email: <a href="mailto:{$aDvs.email}?utm_medium=popup&utm_campaign=dvs_deactivated_popup&utm_source=email_link">{$aDvs.email}</a></p>
     </div>
