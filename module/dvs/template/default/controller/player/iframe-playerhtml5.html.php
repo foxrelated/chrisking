4bcbcce29d851b1d0fe234ae1adf9ec739786e26
Collin Davis <collin.e.davis@gmail.com>
2017-07-10 18:37:40 -0700

    firefox fix: revert

--- module/dvs/template/default/controller/player/iframe-playerhtml5.html.php
+++ module/dvs/template/default/controller/player/iframe-playerhtml5.html.php
@@ -117,12 +117,12 @@ color:#666 !important;
      
 #overview_playlist {l}
 height: 110px !important;
-width: 800px !important;
+{*width: 800px !important;*}
 {r} 
 
-#overview_playlist ul {l}
+{* #overview_playlist ul {l}
 width: 800px !important;
-{r} 
+{r} *}
           
 #overview_playlist ul li img {l}
 width: 149px !important;
