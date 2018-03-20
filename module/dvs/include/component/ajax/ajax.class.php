9fae41c084d2cb39f067a70a6c9cffb8f5a81d15
Won Ko <wko@wheelstv.net>
2018-03-20 16:10:32 -0400

    Call To Actions: [03/20/2018 Tue 0406PM] -removed codes that were added by merging develop branch: --all of these validations are not needed as there are validation functions exist in a template page --removed interval timeout for modals -added back the styling for modals that were deleted by merging develop branch

--- module/dvs/include/component/ajax/ajax.class.php
+++ module/dvs/include/component/ajax/ajax.class.php
@@ -1449,36 +1449,29 @@ class Dvs_Component_Ajax_Ajax extends Phpfox_Ajax
 
 		if (!$aVals['contact_name'] && Phpfox::getParam('dvs.get_price_validate_name'))
 		{
-			Phpfox_Error::set(Phpfox::getPhrase('dvs.please_enter_your_name'). ' ');
+//			Phpfox_Error::set(Phpfox::getPhrase('dvs.please_enter_your_name'). ' ');
 			$bIsError = true;
 		}
 		if (!$aVals['contact_email'] && Phpfox::getParam('dvs.get_price_validate_email'))
 		{
-			Phpfox_Error::set(Phpfox::getPhrase('dvs.please_enter_your_email_address'). ' ');
+//			Phpfox_Error::set(Phpfox::getPhrase('dvs.please_enter_your_email_address'). ' ');
 			$bIsError = true;
 		}
 		if (!$aVals['contact_phone'] && Phpfox::getParam('dvs.get_price_validate_phone'))
 		{
-			Phpfox_Error::set(Phpfox::getPhrase('dvs.please_enter_your_phone_number'). ' ');
+//			Phpfox_Error::set(Phpfox::getPhrase('dvs.please_enter_your_phone_number'). ' ');
 			$bIsError = true;
 		}
-		if (!$aVals['contact_zip'] && Phpfox::getParam('dvs.get_price_validate_zip_code'))
-		{
-			Phpfox_Error::set(Phpfox::getPhrase('dvs.please_enter_your_zip_code'). ' ');
-			$bIsError = true;
-		}
-		if (!$aVals['contact_comments'] && Phpfox::getParam('dvs.get_price_validate_comments'))
-		{
-			Phpfox_Error::set(Phpfox::getPhrase('dvs.please_enter_comments'). ' ');
-			$bIsError = true;
-		}
+
+
 
 		if (!$bIsError)
 		{
-
 			$this->call("$('#contact_dealer').hide();");
 			$this->call("$('#dvs_contact_success').show();");
-			$this->call("setTimeout(function() { tb_remove(); }, 3000);");
+                        $this->call("$('.js_box_title').hide();");
+                        $this->call("$('.js_box_content').css('min-height', '280px');");
+			/*$this->call("setTimeout(function() { tb_remove(); }, 3000);");*/
 
 			$recipients = Phpfox::getService('dvs.email')->sendDealerContactEmail($aVals);
 
