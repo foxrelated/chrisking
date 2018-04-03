99d446ef65e3ea90aaab9456009e2aa465ce5e4c
Won Ko <wko@wheelstv.net>
2018-04-03 12:38:02 -0400

    Mobile Responsiveness [04/03/2018 Tue 12:37 PM] -uploading a test page for VTD and embed player

---
+++ test1.php
@@ -0,0 +1,30 @@
+<?php ?>
+
+<html>
+    <body>
+        <div class="dvs_vin_btn" vin="JF1ZNAA18H8711003">JF1ZNAA1H8</div>
+        <div id="dvs_wrapper"></div>
+        <script type="text/javascript" src="//www.wtvdvs-dev.com/module/dvs/static/jscript/embed.js"></script>
+        <script type="text/javascript">
+            WTVDVS.render_iframe({
+                "id" : "dvs_wrapper",
+                "width" : 952,
+                "height" : 1000,
+                "iframeUrl" : "//showroomtest.wtvdvs-dev.com//iframe/"
+            });
+        </script>
+        
+        
+
+
+        <script type="text/javascript" src="//www.wtvdvs-dev.com/module/dvs/static/jscript/vin.js"></script>
+        <script type="text/javascript">
+            WTVVIN.init({
+                "dvs" : 17,
+                "apiUrl" : "//www.wtvdvs-dev.com",
+                "styleUrl" : "//dvs.wtvdvs-dev.com/vin/style/id_17/",
+                "scriptUrl" : "//dvs.wtvdvs-dev.com/vin/script/id_17/"
+            });
+        </script>
+    </body>
+</html>
